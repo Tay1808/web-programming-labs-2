46fe5dfa2189a24a5a3199813aed0388656273e9
Таня Лоринец <Lorinecz@yandex.ru>
2024-09-16 22:16:00 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ app = Flask(__name__)
 def not_found(err):
     return "нет такой страницы", 404
 
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return '''<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
-                <a href = "/author">author</a>
+                <a href = "/lab1/author">author</a>
             </body> 
         </html>''', 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
         }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
-    name = "Лоринец Татьяна"
+    name = "Лоринец Татьяна Андреевна"
     group = "ФБИ-22"
     faculty = "ФБ"
 
@@ -30,7 +30,7 @@ def author():
                <p>Студент: ''' + name + '''</p>
                <p>Группа: ''' + group + '''</p>
                <p>Факультет: ''' + faculty + '''</p>
-               <a href = "/web">web</a>
+               <a href = "/lab1/web">web</a>
             </body>
         </html>'''    
 
@@ -51,7 +51,6 @@ def oak():
     </body>
 </html>
 '''
-
 count = 0
 
 @app.route('/lab1/counter')
@@ -81,10 +80,9 @@ def clean_counter():
 </html>
 '''
 
-
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
