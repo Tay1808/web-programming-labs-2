ac021209bab717316991568fdebb7b66259bda8e
Таня Лоринец <Lorinecz@yandex.ru>
2024-09-24 20:29:13 +0700

    Имя студента

--- app.py
+++ app.py
@@ -438,6 +438,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
-
-
+    name = 'Танюшка Лоринец'
+    return render_template('example.html', name=name) 
